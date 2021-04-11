f8f6f0ef3e16651e2fa31134163111d77803224d
gleydistonbr <gleydistonbarreto@gmail.com>
2021-04-11 20:13:23 -0300

    primeiro app flutter

---
+++ App-01.dart
@@ -0,0 +1,13 @@
+import 'package: flutter / material.dart' ;
+
+void main () => runApp ( MyApp ());
+
+class MyApp extends StatelessWidget {
+  @sobrepor
+  Widget build ( BuildContext context ) { return MaterialApp (
+    title : 'Welcome to Flutter' ,
+    home : Scaffold (
+      appBar : AppBar (
+        title : Text ( 'Welcome to Flutter' ), ),
+      body : Center (
+        child : Text ( 'Hello World ' ), ), ), ); } }
---
+++ aula01-input.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main(){
+  print("IDADE: ");
+  var input = stdin.readLineSync();
+  var idade = int.parse(idade);
+}
